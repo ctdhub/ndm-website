49f104af7f8de5810c357aafa29dc1231ba76908
ctdhub <cay456@gmail.com>
2022-07-04 18:09:02 -0400

    Pages Test

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './index.html',
-        nested: './nested/index.html'
+        giving: './src/pages/giving.html'
       }
     }
   }
